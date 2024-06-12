47e082670a16582f756ccdc28fe6fd2e08574a93
Antonio Rivera <antoniorm80@hotmail.com>
2024-06-11 19:05:16 -0600

    Obteniendo datos de API con fetch

--- app/(landing)/contact/page.tsx
+++ app/(landing)/contact/page.tsx
@@ -5,21 +5,22 @@ export const metadata: Metadata= {
  description: 'Contact Page Decription',
 };
 
-// const fetchAPI = async () => {     
-//   const response = await fetch("http://www.webapi-northwind.somee.com/api/Customers")
-//   const data = await response.json() 
-//   return data;    
-// }
+const fetchAPI = async () => {     
+  const response = await fetch("http://www.webapi-northwind.somee.com/api/Customers")
+  const data = await response.json() 
+  return data;    
+}
 
 
 // export default "async" function ContactPage() {
-export default  function ContactPage() {
-  // const clientes: any[] = await fetchAPI();  
+export default async function ContactPage() {
+  
+  const clientes: any[] = await fetchAPI();  
 
   return (
     <div>
     <span  className="text-7xl" >Contact Page</span> 
-    {/*      
+         
       {  
         clientes.map( (cliente: any) => (
             <ul key={cliente.customerId}>
@@ -27,7 +28,7 @@ export default  function ContactPage() {
             </ul>                
         )) 
       }    
-    */}
+    
     </div>
     
   )
