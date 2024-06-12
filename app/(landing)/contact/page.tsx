3a235c00a9f8257f6c7c59abf1a92792bbae6af3
Antonio Rivera <antoniorm80@hotmail.com>
2024-06-11 18:43:40 -0600

    Creando el website template with NEXT

---
+++ app/(landing)/contact/page.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata= {
+ title: 'Contact Page',
+ description: 'Contact Page Decription',
+};
+
+// const fetchAPI = async () => {     
+//   const response = await fetch("http://www.webapi-northwind.somee.com/api/Customers")
+//   const data = await response.json() 
+//   return data;    
+// }
+
+
+// export default "async" function ContactPage() {
+export default  function ContactPage() {
+  // const clientes: any[] = await fetchAPI();  
+
+  return (
+    <div>
+    <span  className="text-7xl" >Contact Page</span> 
+    {/*      
+      {  
+        clientes.map( (cliente: any) => (
+            <ul key={cliente.customerId}>
+                <li> { cliente.city } </li>
+            </ul>                
+        )) 
+      }    
+    */}
+    </div>
+    
+  )
+}
